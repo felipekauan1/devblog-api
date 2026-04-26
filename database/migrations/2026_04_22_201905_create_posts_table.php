ff0d2ac0dcc75b45e12d037091d9ee617d1b55d9
Felipe Kauãn <felipekauan30@outlook.com>
2026-04-26 10:16:39 -0300

    refactor: configura vínculo de authorId na migration de posts

--- database/migrations/2026_04_22_201905_create_posts_table.php
+++ database/migrations/2026_04_22_201905_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('slug')->unique();
+            $table->foreignId('authorId')->constrained('users')->cascadeOnDelete();
             $table->string('title');
             $table->text('content');
             $table->string('cover')->nullable();
