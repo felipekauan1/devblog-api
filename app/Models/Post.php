5ed1b9434ad2a6b33dc086ca542b0fc9155382a7
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-04 17:37:03 -0300

    chore: realiza testes de relacionamento utilizando Tinker

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,15 @@ class Post extends Model
     const UPDATED_AT = 'updatedAt';
     const CREATED_AT = 'createdAt';
 
+    protected $fillable = [
+        'slug',
+        'authorId',
+        'title',
+        'content',
+        'cover',
+        'status',
+    ];
+
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'authorId ');
